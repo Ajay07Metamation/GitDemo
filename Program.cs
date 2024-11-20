7b7fa94b056900fdef7220762959ece63bbd289b
Ajay <ajaykarthikeyan.n@metamation.com>
2024-11-20 14:26:00 +0530

    Added functionality to check prime numbers

--- Program.cs
+++ Program.cs
@@ -1,4 +1,11 @@
 ﻿using static System.Console;
 bool IsEven (int num) => num % 2 == 0;
+bool IsPrime (int num) {
+      if (num <= 1 || num % 2 == 0) return false;
+      if (num == 2) return true;
+      for (int i = 3; i <= Math.Sqrt (num); i += 2)
+         if (num % i == 0) return false;
+      return true;
+}
 int num = 5;
-WriteLine ($"The number {num} is {IsEven (num)}");
+WriteLine ($"The number {num} is {IsEven (num)}\nThe number {num} is {IsPrime (num)}");
