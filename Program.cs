71c0e5c69ffdb22b85bb276759f188404d270cd0
Ajay <ajaykarthikeyan.n@metamation.com>
2024-11-20 16:17:25 +0530

    Modified

--- Program.cs
+++ Program.cs
@@ -1,4 +1,4 @@
 ﻿using static System.Console;
-bool IsEven (int num) => num % 2 == 0;
+bool IsEven (int num,int n) => num % 2 == 0;
 int num = 5;
-WriteLine ($"The number {num} is {IsEven (num)}");
+WriteLine ($"The number {num} is {IsEven (num,1)}");
