c9dfa41828e213fe8f092dcfde69c6b5c067eaf4
Ajay <ajaykarthikeyan.n@metamation.com>
2024-11-20 14:38:16 +0530

    Added functionality to calculate factorial

--- Program.cs
+++ Program.cs
@@ -1,4 +1,10 @@
 ﻿using static System.Console;
 bool IsEven (int num) => num % 2 == 0;
+long Factorial (int num) {
+   if (num < 0) throw new ArgumentException ("Factorial is not defined for negative numbers.");
+   if (num == 0 || num == 1) return 1;
+   return num * Factorial (num - 1);
+}
 int num = 5;
-WriteLine ($"The number {num} is {IsEven (num)}");
+WriteLine ($"The number {num} is {(IsEven (num) ? "Even" : "Odd")}");
+WriteLine ($"The factorial of {num} is {Factorial (num)}");
